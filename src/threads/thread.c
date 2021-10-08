0b0e8d6024191a9fa8e35434656d0faf6e99fa17
문규 심 <moon3574@gmail.com>
2021-10-08 11:16:34 +0900

    * Pintos    - Alarm: init sleep ticks value

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -469,6 +469,8 @@ init_thread (struct thread *t, const char *name, int priority)
   t->priority = priority;
   t->magic = THREAD_MAGIC;
 
+  t->sleep_ticks = 0;
+
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
